d0bbd708ac5bf37281f21b14463c8c7481480c1a
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-02-02 04:33:21 +0000

    Remove unnecessary UI.

--- src/ui.js
+++ src/ui.js
@@ -1,10 +1,6 @@
 // The enter function for creating all ui components.
 TOPPANO.createUI = function(model) {
     TOPPANO.createMenu(model['menu']);
-    TOPPANO.createSummary(model['summary']);
-    TOPPANO.createFullscreenBtn()
-    TOPPANO.createFBShareBtn();
-    TOPPANO.createEmbeddedLink();
 };
 
 // Create a sidebar menu.
@@ -17,51 +13,6 @@ TOPPANO.createMenu = function(menu) {
     $('#menu .sidebar-content-share-width').on('input', TOPPANO.onEmbeddedLinkChange);
     $('#menu .sidebar-content-share-height').on('input', TOPPANO.onEmbeddedLinkChange);
     TOPPANO.onEmbeddedLinkChange();
-};
-
-// Create a component for showing summary of the model.
-TOPPANO.createSummary = function(summary) {
-    var id = 'summary';
-
-    TOPPANO.createSummaryUI(id);
-    TOPPANO.fillSummaryContent(id, summary);
-    TOPPANO.addSummaryListener(id, summary);
-};
-
-// Create the UI of Summary.
-TOPPANO.createSummaryUI = function(id) {
-};
-
-// Fill the content of Summary.
-TOPPANO.fillSummaryContent = function(id, prop) {
-    $.each(prop, function(input, value) {
-        $('#' + id + '-' + input).val(value);
-    });
-};
-
-// Add Listeners of Summary.
-TOPPANO.addSummaryListener = function(id, prop) {
-    $('#summary-main .ui-collapsible-heading-toggle').on('click', TOPPANO.onSummaryMainClick);
-    $('#summary-btn').on('click', TOPPANO.onSummaryBtnClick);
-};
-
-// Create a button for enter/exit fullscreen mode.
-TOPPANO.createFullscreenBtn = function() {
-    $('#fullscreen-btn').on('click', TOPPANO.onFullscreenBtnClick);
-};
-
-// Create a Facebook share button.
-TOPPANO.createFBShareBtn = function() {
-    $('#fb-share-btn').on('click', TOPPANO.onFBShareBtnClick);
-};
-
-// Create a block for sharing embedded link.
-TOPPANO.createEmbeddedLink = function() {
-    /*
-    TOPPANO.onEmbeddedLinkChange();
-    $('#embedded-link-width').on('input', TOPPANO.onEmbeddedLinkChange);
-    $('#embedded-link-height').on('input', TOPPANO.onEmbeddedLinkChange);
-    */
 };
 
 // Initialize Facebook SDK.
@@ -111,10 +62,6 @@ TOPPANO.ui = {
         linkMinHeight: 160,
         currentClickedIcon: null
     },
-    // Summary block paramters
-    summaryUI: {
-        animateDelay: 1500
-    },
     // Facebook SDK parameters
     fbSdkParams: {
         appId: '226223091041998',
@@ -123,13 +70,6 @@ TOPPANO.ui = {
     // Google API parameters
     googleApiParams: {
         shortUrlKey: 'AIzaSyDh1jky-M2BSe5Dnq2CdZiqadfB7t0Qan4'
-    },
-    // Embedded Link parameters
-    /*
-    embeddedLinkUI: {
-        minWidth: 240,
-        minHeight: 160
     }
-    */
 };
 
--- src/uiListener.js
+++ src/uiListener.js
@@ -91,37 +91,6 @@ TOPPANO.getTransitionEndEventName = function() {
     }
 };
 
-// Listener for clicking Summary button.
-TOPPANO.onSummaryBtnClick = function(event) {
-    var summaryBtn = $('#summary-btn');
-    var summaryMain = $('#summary-main');
-
-    // When the main block is about to be closed, we should collapse
-    // the main block if it is expanded.
-    if(summaryBtn.hasClass('ui-icon-arrow-r') && !summaryMain.hasClass('ui-collapsible-collapsed')) {
-        $('#summary-main .ui-collapsible-heading-toggle').trigger('click');
-        summaryMain.delay(TOPPANO.ui.summaryUI.animateDelay);
-    }
-    summaryMain.animate({width: 'toggle' }, TOPPANO.ui.summaryUI.animateDelay, function() {
-        summaryBtn.toggleClass('ui-icon-arrow-r');
-        summaryBtn.toggleClass('ui-icon-arrow-l');
-    });
-};
-
-// Listener for clicking Summary head block.
-TOPPANO.onSummaryMainClick = function(event) {
-    $('.ui-collapsible-content', $('#summary-main')).slideToggle(TOPPANO.ui.summaryUI.animateDelay);
-};
-
-// Listener for clicking fullscreen button.
-TOPPANO.onFullscreenBtnClick = function(event) {
-    if($.fullscreen.isFullScreen()) {
-        $.fullscreen.exit();
-    } else {
-        $('body').fullscreen();
-    }
-};
-
 // Listener for clicking Facebook sharing button.
 TOPPANO.onFBShareBtnClick = function(event) {
     // Steps for sharing to Facebook:
@@ -207,14 +176,6 @@ TOPPANO.onEmbeddedLinkChange = function(event) {
         '" style="border: none" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
     $('#menu .sidebar-content-share-output').html(link);
 };
-TOPPANO.onSGImgClick = function(prop) {
-    TOPPANO.transitNode(prop.nodeId, prop.lng, prop.lat, prop.fov);
-};
-
-// Transit current node to another node.
-TOPPANO.transitNode = function(targetNodeId, lng, lat, fov) {
-    TOPPANO.changeView(targetNodeId, lng, lat, fov);
-};
 
 // Convert a base64-encoded image to a Blob.
 TOPPANO.base64toBlob = function(dataUrl) {
@@ -235,5 +196,14 @@ TOPPANO.base64toBlob = function(dataUrl) {
     }
 
     return new Blob([ia], {type:mimeString});
+};
+
+// Listener for clicking fullscreen button.
+TOPPANO.onFullscreenBtnClick = function(event) {
+    if($.fullscreen.isFullScreen()) {
+        $.fullscreen.exit();
+    } else {
+        $('body').fullscreen();
+    }
 };
 
