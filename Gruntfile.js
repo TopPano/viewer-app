4358e8f8c7ea8af0f5752a66648a84047152ea7b
Jia-Kuan Su <jiakuan.su@toppano.in>
2016-03-17 09:33:47 +0000

    Enhance development environments with Grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,104 @@
 
 module.exports = function(grunt) {
     grunt.initConfig({
+        eslint: {
+            target: [ 'src/*.js' ]
+        },
+
         uglify: {
-            dist: {
+            jslib: {
+                options: {
+                    sourceMap: true
+                },
                 files: {
-                    'dist/toppano.min.js': [
+                    'dist/lib.min.js': [
                         'src/lib/three.min.js',
                         'src/lib/detect.min.js',
                         'src/lib/CanvasRenderer.js',
                         'src/lib/Projector.js',
                         'src/lib/stats.js',
+                        'src/lib/jquery-1.11.3.min.js',
+                        'src/lib/screenfull.min.js',
+                        'src/lib/clipboard.min.js',
+                        'src/lib/jquery.fullscreen-0.4.1.min.js',
+                        'src/lib/jquery.magnific-popup.min.js',
+                        'src/lib/js.cookie.min.js'
+                    ]
+                }
+            },
+            jsapp: {
+                options: {
+                    sourceMap: true
+                },
+                files: {
+                    'dist/app.min.js': [
                         'src/api.js',
                         'src/class.js',
                         'src/functions.js',
+                        'src/listener.js',
                         'src/view.js',
-                        'src/listener.js'
+                        'src/ui.js',
+                        'src/uiListener.js',
+                        'src/user.js',
+                        'src/application.js'
                     ]
                 }
             }
         },
+
+        cssmin: {
+            dist: {
+                options: {
+                },
+                files: {
+                    'dist/all.min.css': [ 'css/*.css' ]
+                }
+            }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                    require('postcss-cssnext')()
+                ]
+            },
+            dist: {
+                src: 'dist/all.min.css'
+            }
+        },
+
+        shell: {
+            target: {
+                command: 'cp -r css/images dist/'
+            }
+        },
+
+        watch: {
+            jslib: {
+                files: [ 'src/lib/*.js' ],
+                tasks: [ 'uglify:jslib' ]
+            },
+            jsapp: {
+                files: [ 'src/*.js' ],
+                tasks: [ 'eslint', 'uglify:jsapp' ]
+            },
+            css: {
+                files: [ 'css/*.css' ],
+                tasks: [ 'css' ]
+            }
+        }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['uglify']); // Default grunt tasks maps to grunt
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-eslint');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-shell');
 
+    grunt.registerTask('js', [ 'eslint', 'uglify' ]);
+    grunt.registerTask('css', [ 'cssmin', 'postcss', 'shell' ]);
+    grunt.registerTask('default', [ 'js', 'css' ]);
+    grunt.registerTask('dev', [ 'watch' ]);
 };
+
